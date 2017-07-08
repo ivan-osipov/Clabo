68da9404604ed64d0c9ba5d0d5d32fcbad52b60a
Ivan Osipov <i.osipov.develop@gmail.com>
2017-07-08 23:46:19 +0400

    updated a sender sync by chats

--- src/main/kotlin/com/github/ivan_osipov/clabo/api/internal/QueueBasedSender.kt
+++ src/main/kotlin/com/github/ivan_osipov/clabo/api/internal/QueueBasedSender.kt
@@ -1,6 +1,9 @@
 package com.github.ivan_osipov.clabo.api.internal
 
 import com.github.ivan_osipov.clabo.api.output.dto.SendParams
+import com.github.ivan_osipov.clabo.utils.ChatId
+import com.google.common.base.Preconditions.checkArgument
+import com.google.common.collect.Maps
 import com.google.common.collect.Queues
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -8,16 +11,15 @@ import java.util.*
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
 import java.util.concurrent.Semaphore
+import java.util.concurrent.atomic.AtomicBoolean
 
 internal class QueueBasedSender(val apiInteraction: TelegramApiInteraction) {
 
-    private val outputQueue: Queue<SendParams> = Queues.newConcurrentLinkedQueue()
+    val messageProcessingSemaphore = Semaphore(0)
 
-    private val queueSemaphore: Semaphore = Semaphore(0)
+    private val chatOutputMap: MutableMap<ChatId, ChatInfo> = Maps.newHashMap()
 
-    private val resultTakingSemaphore: Semaphore = Semaphore(1)
-
-    private val workerExecutor: Executor = Executors.newSingleThreadExecutor()
+    private val workerExecutor: Executor = Executors.newSingleThreadExecutor() //executes thread for queue processing
 
     private val logger: Logger = LoggerFactory.getLogger(QueueBasedSender::class.java)
 
@@ -26,10 +28,15 @@ internal class QueueBasedSender(val apiInteraction: TelegramApiInteraction) {
         logger.debug("QueueBasedSender is initialized")
     }
 
-    fun send(sendParams: SendParams) {
-        outputQueue.add(sendParams)
-        logger.debug("New message in queue")
-        queueSemaphore.release()
+    fun send(message: SendParams) {
+        var chatOutput = chatOutputMap[message.chatId]
+        if (chatOutput == null) {
+            chatOutput = ChatInfo(message.chatId)
+            chatOutputMap[message.chatId] = chatOutput
+        }
+        chatOutput.outputQueue.add(message)
+        logger.debug("New message in queue (chatId: ${chatOutput.chatId})")
+        notifyWorker()
     }
 
     inner class WorkerThread : Runnable {
@@ -38,22 +45,45 @@ internal class QueueBasedSender(val apiInteraction: TelegramApiInteraction) {
 
         override fun run() {
             while (true) {
-                val nextOutputMessage: SendParams? = outputQueue.poll()
+                val chatsForProcessing = chatOutputMap.filter { it.value.canBeProcessed() }.values
+                if(chatsForProcessing.isEmpty()) {
+                    messageProcessingSemaphore.acquire()
+                    logger.debug("The worker thread is unlocked")
+                    continue
+                }
+                for (chatForProcessing in chatsForProcessing) {
 
-                if(nextOutputMessage == null) {
-                    logger.debug("Output queue is empty")
-                    queueSemaphore.acquire()
-                } else {
+                    val message = chatForProcessing.outputQueue.peek()
                     logger.debug("Message sending")
-                    resultTakingSemaphore.acquire()
 
-                    apiInteraction.sendMessage(nextOutputMessage, {
-                        resultTakingSemaphore.release()
-                    }, {
-                        resultTakingSemaphore.release()
-                    })
+                    val lockIsSuccessful = chatForProcessing.resultTalkingMutex.compareAndSet(false, true)
+
+                    if (lockIsSuccessful) {
+                        val headMessage = chatForProcessing.outputQueue.remove()
+                        checkArgument(headMessage == message, "Incorrect message as the queue head")
+
+                        apiInteraction.sendMessage(message, {
+                            chatForProcessing.resultTalkingMutex.set(false)
+                            notifyWorker()
+                        }, {
+                            chatForProcessing.resultTalkingMutex.set(false)
+                            notifyWorker()
+                        })
+                    }
                 }
             }
         }
+    }
+
+    private fun notifyWorker() {
+        if (messageProcessingSemaphore.hasQueuedThreads()) {
+            messageProcessingSemaphore.release()
+        }
+    }
+
+    private class ChatInfo(val chatId: ChatId,
+                   val resultTalkingMutex: AtomicBoolean = AtomicBoolean(false),
+                   val outputQueue: Queue<SendParams> = Queues.newLinkedBlockingQueue()) {
+        fun canBeProcessed() = !resultTalkingMutex.get() && outputQueue.isNotEmpty()
     }
 }
