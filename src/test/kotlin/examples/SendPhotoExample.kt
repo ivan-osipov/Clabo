bf02cc39d9a28de92a08648e205ccaad62cf48ec
Ivan Osipov <i.osipov.develop@gmail.com>
2017-09-09 17:43:55 +0400

    #9 updated example

--- src/test/kotlin/examples/SendPhotoExample.kt
+++ src/test/kotlin/examples/SendPhotoExample.kt
@@ -4,12 +4,20 @@ import com.github.ivan_osipov.clabo.dsl.bot
 import com.github.ivan_osipov.clabo.dsl.props
 import java.io.File
 
-fun main(args: Array<String>) {
-    val photoFile = File(Main::class.java.getResource("test.png").file)
+private val photoFile = File(Main::class.java.getResource("test.png").file)
 
+fun main_sync(args: Array<String>) {
     bot(props(Main::class, "bot.properties")) longPolling {
         onStart {
             sendPhotoSync(it.message.chat.id, photoFile /*or file id or url*/)
+        }
+    }
+}
+
+fun main_anync(args: Array<String>) {
+    bot(props(Main::class, "bot.properties")) longPolling {
+        onStart {
+            sendPhoto(it.message.chat.id, photoFile /*or file id or url*/)
         }
     }
 }
